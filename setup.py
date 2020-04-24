c3cd901f39b513e9d3d9ee2fde3bec8413a816e2
ephreal <tjannx@hotmail.com>
2020-04-24 11:52:11 -0700

    Added in description and bumped to v0.2.0-alpha

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 from distutils.core import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 
 setup(
       name='catapi.py',
       packages=['catapi'],
-      version='0.1.1-alpha',
+      version='0.2.0-a',
       license='MIT',
       description='A python wrapper for thecatapi.com',
+      long_description=long_description,
+      long_description_content_type="text/x-rst",
       author='Ephreal',
       author_email='contact@mylifeneeds.management',
       url='https://github.com/ephreal/catapi.py',
-      download_url='https://github.com/ephreal/catapi.py/archive/v0.1.1-alpha.tar.gz',
+      download_url='https://github.com/ephreal/catapi.py/archive/0.2.0-alpha.tar.gz',
       keywords=['cats', 'catapi', ],
       install_requires=[
             'aiohttp',
