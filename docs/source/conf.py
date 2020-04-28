3da43317dd5c4ad705768ca34abead9cbc4db2c2
ephreal <tjannx@hotmail.com>
2020-04-28 10:53:20 -0700

    Explicitly set master_source to fix failing doc build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,6 +20,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 project = 'catapi.py'
 copyright = '2020, ephreal'
 author = 'ephreal'
+master_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
 release = '0.3.5'
